ebb10230906b789e363f6121a978947acfaed05c
kaviyarasu134 <kaviyarasuravi5@gmail.com>
2024-01-20 12:03:16 +0530

    jjjj

---
+++ Realme.java
@@ -0,0 +1,33 @@
+package org.mobile;
+
+public class Realme {
+	
+	
+	public static void main(String[] args) {
+		
+		
+		for(int i=1;i<=100;i++) {
+			
+			
+			
+			
+			
+		
+				System.out.println(i);
+		
+		
+		
+		
+		
+	
+		
+		
+	}
+}
+}
+
+		
+		
+		
+		
+		
